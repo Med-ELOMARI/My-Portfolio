67d745b7cd2f24b24d42aa52f120af928eacad56
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2019-08-28 19:01:40 +0200

    initial Commit

---
+++ app/data.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+"""
+Fully Coded App by AppSeed.us
+License: commercial
+Read more at https://appseed.us/pricing
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+# Resources used 
+class Data:
+
+    copy       = 'AppSeed'
+
+    email      =  'support@appseed.us'
+    design     =  'Indivisible by Pixelarity'
+    
+    seo_title  = 'Flask App - Indivisible design by Pixelarity | AppSeed'
+    seo_desc   = 'Flask app with Indivisible design crafted by top Pixelarity'
+    seo_keys   = 'jamstack, indivisible, html5 up, pixelarity, appseed'
+
+    product    = 'https://appseed.us/apps/flask-apps/flask-indivisible-pixelarity' 
+    github     = 'https://github.com/app-generator/priv-flask-indivisible'
+    docs       = 'https://docs.appseed.us/apps/flask-apps/flask-indivisible-pixelarity/'
+    appseed    = 'https://appseed.us'      
+    generator  = 'https://appseed.us/app-generator'
+    blog       = 'https://blog.appseed.us/'
+    facebook   = 'https://facebook.com/webappseed'
+    twitter    = 'https://twitter.com/webappseed'
+    instagram  = 'https://instagram.com/webappseed'
+    support    = 'https://appseed.us/support'
+    discord    = 'https://discord.gg/fZC6hup'
