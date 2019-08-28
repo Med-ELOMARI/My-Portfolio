ec217c7b9ca66edf4d7e812e9919ee95e41bb639
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2019-08-28 19:55:22 +0200

    first upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 	db.create_all()
 
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
+	app.run(port=port, debug=True)
 	#app.run(ssl_context='adhoc')
 
     
